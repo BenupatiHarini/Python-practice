c431f559ecec340b45829db4cf25a5524ca9e37f
Benupati Harini <benupatiharini@gmail.com>
2025-08-01 20:11:24 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+const cors = require('cors');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server, {
+  cors: {
+    origin: "*", // Allow any frontend for now
+  }
+});
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.send("Server is running!");
+});
+
+// Sample socket connection
+io.on('connection', (socket) => {
+  console.log('A user connected:', socket.id);
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected:', socket.id);
+  });
+});
+
+const PORT = 3000;
+server.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
