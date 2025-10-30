a561c8d4f1204f1873f1be17d79a70b95eb09d89
Benupati Harini <benupatiharini@gmail.com>
2025-10-30 21:33:38 +0530

    Add files via upload

---
+++ prblm79.py
@@ -0,0 +1,23 @@
+def rotate(matrix):
+    n = len(matrix)
+
+    # Step 1: Transpose the matrix
+    for i in range(n):
+        for j in range(i, n):
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+    
+    # Step 2: Reverse each row
+    for i in range(n):
+        matrix[i].reverse()
+
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+rotate(matrix)
+
+print("Rotated Matrix:")
+for row in matrix:
+    print(row)
